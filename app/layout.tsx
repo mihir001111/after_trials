da9558e60c05719044224e08130cb5c7abbe8804
Mihir Ranjan <mihirranjan730@gmail.com>
2026-02-08 01:00:03 +0100

    Add navigation progress bar (nextjs-toploader)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter, Outfit } from 'next/font/google'
 import './globals.css'
 import clsx from 'clsx'
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const outfit = Outfit({ subsets: ['latin'], variable: '--font-outfit' })
@@ -20,7 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, outfit.variable, "antialiased")}>{children}</body>
+      <body className={clsx(inter.className, outfit.variable, "antialiased")}>
+        <NextTopLoader
+          color="#000000"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #000000,0 0 5px #000000"
+        />
+        {children}
+      </body>
     </html>
   )
 }
