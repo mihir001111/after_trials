9662916cb4995965aac7ce09ec312acf11727d0a
Mihir Ranjan <mihirranjan730@gmail.com>
2026-02-11 17:34:03 +0100

    chore: optimize build to reduce memory usage

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 
 
   headers: async () => {
